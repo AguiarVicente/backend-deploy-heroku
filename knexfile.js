cc4ddf98a17d470f79a53b78e23043a0635f1170
AguiarVicente <aguiar14564@gmail.com>
2020-08-13 21:43:16 -0300

    ajustes .env

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,13 @@ const db = require('.env')
 
 module.exports = {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: {
+        host: 'ec2-52-204-20-42.compute-1.amazonaws.com',
+        port: 5432,
+        database: 'dajt46c7k86gkq',
+        user: 'gqygbiojtwbcdy',
+        password: 'f1c4d83a16b3e38b5b5dd45df6129ef319973bde62301bfe99ba7f527e9c3d28'
+    },
     pool: {
       min: 2,
       max: 10
